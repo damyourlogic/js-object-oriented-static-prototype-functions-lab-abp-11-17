425128b4f5d7b801c4ee7550b7522e2dd3f81470
Amy Jackson <aj81623@gmail.com>
2017-11-16 23:03:08 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -8,7 +8,11 @@ IcebreakerResponse.BatchCreate = function(emails){
     returns.push(new IcebreakerResponse(email))
   })
 
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> 0f52ba8c01fb976c4dcacf4a3b702766d342d9d4
 
 
   return returns
